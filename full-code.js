0aaecf25112838086ca2da0f4628d4c53fd85912
Володимир Крісанов <volodimirkrisanov@gmail.com>
2026-01-21 14:14:26 +0100

    Update full-code.js

--- full-code.js
+++ full-code.js
@@ -1,61 +1,59 @@
 'use strict';
 
-// Ітерування циклами - функція sum(...args)
-// Реалізація п'ятьма різними способами
-
-const sum = (...args) => {
-  // Перевірка на порожні аргументи
-  if (args.length === 0) return 0;
-  
-  const results = [];
-  
-  // 1. Цикл for
-  let sumFor = 0;
+// 1. Цикл for
+const sumFor = (...args) => {
+  let result = 0;
   for (let i = 0; i < args.length; i++) {
-    sumFor += args[i];
+    result += args[i];
   }
-  results.push(sumFor);
-  
-  // 2. Цикл for..of
-  let sumForOf = 0;
-  for (const value of args) {
-    sumForOf += value;
-  }
-  results.push(sumForOf);
-  
-  // 3. Цикл while
-  let sumWhile = 0;
-  let index = 0;
-  while (index < args.length) {
-    sumWhile += args[index];
-    index++;
-  }
-  results.push(sumWhile);
-  
-  // 4. Цикл do..while
-  let sumDoWhile = 0;
-  let counter = 0;
-  if (args.length > 0) {
-    do {
-      sumDoWhile += args[counter];
-      counter++;
-    } while (counter < args.length);
-  }
-  results.push(sumDoWhile);
-  
-  // 5. Метод Array.prototype.reduce()
-  const sumReduce = args.reduce((accumulator, current) => accumulator + current, 0);
-  results.push(sumReduce);
-  
-  return results;
+  return result;
 };
 
-// Тестування функції sum
-console.log('sum(5, 3, 8):', sum(5, 3, 8));
-console.log('sum(0):', sum(0));
-console.log('sum():', sum());
-console.log('sum(2, -2, 2):', sum(2, -2, 2));
-console.log('sum(15, -3, -3, -3):', sum(15, -3, -3, -3));
+// 2. Цикл for..of
+const sumForOf = (...args) => {
+  let result = 0;
+  for (const value of args) {
+    result += value;
+  }
+  return result;
+};
+
+// 3. Цикл while
+const sumWhile = (...args) => {
+  let result = 0;
+  let index = 0;
+  while (index < args.length) {
+    result += args[index];
+    index++;
+  }
+  return result;
+};
+
+// 4. Цикл do..while
+const sumDoWhile = (...args) => {
+  let result = 0;
+  if (args.length === 0) return 0;
+  
+  let counter = 0;
+  do {
+    result += args[counter];
+    counter++;
+  } while (counter < args.length);
+  
+  return result;
+};
+
+// 5. Метод Array.prototype.reduce()
+const sum = (...args) => {
+  return args.reduce((accumulator, current) => accumulator + current, 0);
+};
+
+// Тестування
+console.log('sum(5, 3, 8):', sum(5, 3, 8)); // 16
+console.log('sum(0):', sum(0)); // 0
+console.log('sum():', sum()); // 0
+console.log('sum(2, -2, 2):', sum(2, -2, 2)); // 2
+console.log('sum(15, -3, -3, -3):', sum(15, -3, -3, -3)); // 6
 
 // Ітерування по двумірному масиву - знайти максимальний елемент
 const max = (matrix) => {
@@ -73,7 +71,7 @@ const max = (matrix) => {
 };
 
 const maxElement = max([[2, 4, 6], [8, 10, 12], [14, 16, 18]]);
-console.log('Max element:', maxElement);
+console.log('Max element:', maxElement); // 18
 
 // Ітерування об'єктів-справочників
 const persons = {
@@ -83,15 +81,14 @@ const persons = {
   tesla: { born: 1856, died: 1943 },
 };
 
-const calculateAges = (people) => {
-  const ages = {};
+const ages = (people) => {
+  const result = {};
   
   for (const name in people) {
     const person = people[name];
-    ages[name] = person.died - person.born;
+    result[name] = person.died - person.born;
   }
   
-  return ages;
+  return result;
 };
-
-console.log('Ages:', calculateAges(persons));
+console.log('Ages:', ages(persons));
